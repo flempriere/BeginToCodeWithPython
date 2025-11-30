b2b827a8df89d2fe553e5eb536a95693e3242401
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-11-30 12:25:04 +0000

    Built site for gh-pages

---
+++ 01_ProgrammingFundamentals/04_WorkingWithVariables/Examples/03_ConfigurableEggTimer/ConfigurableEggTimer.py
@@ -0,0 +1,14 @@
+# Example 4.3: Configurable Egg Timer
+# Reads in a user specified time to set the timer for
+
+import time
+
+time_text = input("Enter the cooking time in seconds: ")
+time_int = int(time_text)
+
+print("Put the egg in boiling water now")
+input("Press enter to continue once the egg is in...")
+
+time.sleep(time_int)
+
+print("Take the egg out now")
