21bb81023de6ee5d57166997f6fcafa591c704f0
Felix Lempriere <47702867+flempriere@users.noreply.github.com>
2025-12-11 21:04:47 +1030

    Content/chapter7 (#9)
    * Started Content for Chapter 7. Added notes and examples up to (but not including) 'Give Information to Functions using Parameters', updated index.qmd, README.md and _quarto.yml for the new Chapter
    
    * Completed Chapter 7 notes and exercises up to (but not including) BTCInput
    
    * Completed Chapter 7 notes and exercises

---
+++ 01_ProgrammingFundamentals/07_UsingFunctions/Exercises/02_BTCInputApplications/RideSelector.py
@@ -0,0 +1,66 @@
+# Exercise 7.2.5 Ride Selector
+#
+# Uses BTCInput to handle input validation
+
+import BTCInput
+
+run_program = True
+
+while run_program:
+    print("""Welcome to our Theme Park
+        These are the available ride:
+
+        1. Scenic River Cruise
+        2. Carnival Carousel
+        3. Jungle Adventure Water Splash
+        4. Downhill Mountain Run
+        5. The Regurgitator
+        Press 0 to quit the program
+        """)
+
+    ride_number = BTCInput.read_int_ranged(
+        "Please enter the ride number you want: ", 0, 5
+    )
+    if ride_number == 0:
+        run_program = False
+    elif ride_number == 1:
+        print("You have selected Scenic River Cruise")
+        print("There are no age limits for this ride")
+    else:  # need to get the age of the user
+        age = BTCInput.read_int("Please enter your age: ")
+        if age < 1:
+            print("You are too young to go on any rides")
+        elif age > 95:
+            print("You are too old to go on any rides")
+        else:
+            if ride_number == 2:
+                print("You have selected the Carnival Carousel")
+                if age >= 3:
+                    print("You can go on the ride")
+                else:
+                    print("Sorry, you are too young")
+            if ride_number == 3:
+                print("You have selected Jungle Adventure Water Splash")
+                if age >= 6:
+                    print("You can go on the ride")
+                else:
+                    print("Sorry, you are too young")
+            if ride_number == 4:
+                print("You have selected Downhill Mountain Run")
+                if age >= 12:
+                    print("You can go on the ride")
+                else:
+                    print("Sorry, you are too young")
+            if ride_number == 5:
+                print("You have selected The Regurgitator")
+                if age >= 12:
+                    # first check age not too lowe
+                    if age > 70:
+                        # Age is too old
+                        print("Sorry, you are too old")
+                    else:
+                        # In the valid range
+                        print("You can go on the ride")
+                else:
+                    # Age is too young
+                    print("Sorry, you are too young")
