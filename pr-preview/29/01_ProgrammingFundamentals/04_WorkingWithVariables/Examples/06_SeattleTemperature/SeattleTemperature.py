e7f2dbdf1ccf8260524b1e605ce04a3441a06d89
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-02-08 06:25:45 +0000

    Built site for gh-pages

--- pr-preview/29/01_ProgrammingFundamentals/04_WorkingWithVariables/Examples/06_SeattleTemperature/SeattleTemperature.py
+++
@@ -1,9 +0,0 @@
-# Example 4.6: Seattle Temperature
-#
-# Get the current temperature in Seattle
-
-import snaps
-
-temp = snaps.get_weather_temp(latitude=47.61, longitude=-122.33)
-
-print("The temperature in Seattle is:", temp)
