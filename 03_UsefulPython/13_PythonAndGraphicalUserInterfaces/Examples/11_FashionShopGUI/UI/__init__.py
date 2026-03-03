62246748af4f58c5b7a33ee7e5cc740135c7c7a1
flempriere <flempriere@gmail.com>
2026-03-03 19:41:04 +1030

    Added notes, examples and exercises for the Fashion Shop graphical implementation, including an extended implementation of the Final Graphical Fashion Shop Application

---
+++ 03_UsefulPython/13_PythonAndGraphicalUserInterfaces/Examples/11_FashionShopGUI/UI/__init__.py
@@ -0,0 +1,12 @@
+"""
+UI
+
+Contains modules to implement the UI for a Fashion Shop
+
+Modules
+-------
+ShellUI
+    Module providing implementations for a shell-based UI
+GUI
+    Module providing implementations for a graphical user interface
+"""
