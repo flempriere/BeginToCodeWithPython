02ca0cfc190c1dc4b1f6c6b97bff651bc1c05c61
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-03-07 12:17:28 +0000

    Built site for gh-pages

--- pr-preview/32/01_ProgrammingFundamentals/07_UsingFunctions/Exercises/03_BTCInputApplications/FahrenheitToCelsius.py
+++
@@ -1,9 +0,0 @@
-# Exercise 7.2.4: Fahrenheit to Celsius
-#
-# Version of Fahrenheit to Celsius that uses BTCInput
-
-import BTCInput
-
-temperature_fahrenheit = BTCInput.read_float("Enter a temperature in Fahrenheit: ")
-temperature_centigrade = (temperature_fahrenheit - 32) / 1.8
-print("The temperature is", temperature_centigrade, "degrees Celsius")
