bf6d20d52ca0de1ed342bcd86c3680b2319e6250
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-03-08 06:10:57 +0000

    Built site for gh-pages

--- pr-preview/33/01_ProgrammingFundamentals/04_WorkingWithVariables/Examples/02_Greeter/greeter.py
+++
@@ -1,6 +0,0 @@
-# Example 4.2: Greeter
-#
-# A simple greeter that reads in a name and greets that name
-
-name = input("Enter your name please: ")
-print("Hello", name)
--- pr-preview/33/01_ProgrammingFundamentals/06_RepeatingActionsWithLoops/Examples/10_NamePrinter/NamePrinter.py
+++
@@ -1,7 +0,0 @@
-# Example 6.10 Name Printer
-#
-# Prints a collection of names
-
-names = ("Rob", "Mary", "David", "Jenny", "Chris", "Imogen")
-for name in names:
-    print(name)
